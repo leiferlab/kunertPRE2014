fixed some issues with numerical precision. still some to solve though.

kunert.py
@@ -9,10 +9,8 @@ from sys import stdout
 #### Set initial parameters
 ##############################################
 
-# Numer of neurons
-N = 300
 # Number of steps
-M = 3
+M = 30
 # Timestep
 dt = 1
 # Time 0
@@ -20,10 +18,21 @@ t0 = 0
 # Final time
 t1 = t0 + M*dt
 
+# Load the connectome and neurotransmitters, +1 excitatory, -1 inhibitory
+f = open('aconnectome.json','r')
+content = json.load(f)
+Gsyn = np.array(content['chemical'])
+Ggap = np.array(content['electrical'])
+Neurotrans = np.array(content['chemical_sign'])
+f.close()
+
+# Number of neurons
+N = len(Neurotrans)
+
 # Cell
 C = 1e-12 # Membrane capacitance [F]
-Gcell = 1e-11*np.ones(N) # Leakage conductance of membrane [S]
-Ecell = -35e-3*np.ones(N) # Leakage potential [V]
+Gcell = 1e-11*np.ones(N,dtype=np.float64) # Leakage conductance of membrane [S]
+Ecell = -35e-3*np.ones(N,dtype=np.float64) # Leakage potential [V]
 
 # Chemical synapses
 gsyn = 1e-10 # "conductivity" of chemical synapse [Siemens]
@@ -34,16 +43,7 @@ esynexc = 0. # reverse potential for excitatory synapses
 esyninh = -45e-3 # reverse potential for inhibitory synapses
 
 # Electrical synapses
-ggap = 1e-10
-
-# Load the connectome
-# and neurotransmitters, +1 excitatory, -1 inhibitory
-f = open('aconnectome.json','r')
-content = json.load(f)
-Gsyn = np.array(content['chemical'])
-Ggap = np.array(content['electrical'])
-Neurotrans = np.array(content['chemical_sign'])
-f.close()
+ggap = 1e-10 # conductivity of electrical synapse [Siemens]
 
 # Build the Esyn array of the synaptic reverse potentials
 # The index is presynaptic neuron, which determines the neurotransmitter and
@@ -65,9 +65,11 @@ def Seq(ar=ar, ad=ad):
 def Veq(V, S, Ggap=Ggap, Gsyn=Gsyn, Gcell=Gcell, ggap=ggap, gsyn=gsyn, \
         Esyn=Esyn, Ec=Ecell):
     VV = np.repeat([V], V.shape, 0)
-    Y = Ec 
-    Y -= np.sum( Ggap/Gcell*ggap*(VV-V), axis=0 ) 
-    Y -= np.sum( Gsyn/Gcell*gsyn*S*(VV-Esyn), axis=0 )
+    Vcell = Ec 
+    Vsyn = np.sum( Gsyn/Gcell*gsyn*S*(VV-Esyn), axis=0 )
+    Vgap = np.sum( Ggap/Gcell*ggap*(VV-V), axis=0 ) 
+    
+    Y = Vcell - Vsyn - Vgap
         
     return Y
 
@@ -84,11 +86,11 @@ def fY(t, Y, Vth, Iext, Ggap=Ggap, Gsyn=Gsyn, C=C, Gcell=Gcell, ggap=ggap, \
     S = Y[N:]
     
     VV = np.repeat([V], V.shape, 0)
-    Igap = 1./C * np.sum( Ggap*ggap*(VV-V), axis=0 )
-    Isyn = 1./C * np.sum( Gsyn*gsyn*S*(VV-Esyn), axis=0 )
+    Igap = np.sum( Ggap*ggap/C*(VV-V), axis=0 )
+    Isyn = np.sum( Gsyn*gsyn/C*S*(VV-Esyn), axis=0 )
     Icel = 1./C * Gcell*(V-Ec)
     
-    Vdot = -Icel-Igap-Isyn+Iext[int(t)]
+    Vdot = -Icel-Igap-Isyn+Iext[int(t)]  #Iext in units of 1/C
         
     Sdot = ar / ( 1.0+np.exp(-beta*(V-Vth)) ) * (1.-S) - ad*S
     
@@ -104,11 +106,6 @@ def fY(t, Y, Vth, Iext, Ggap=Ggap, Gsyn=Gsyn, C=C, Gcell=Gcell, ggap=ggap, \
 #### Do the calculation
 ##############################################
 
-# Initial guess for the voltages
-#V = -12e-3*np.ones(N)
-f = open('V0.json','r')
-V = np.array(json.load(f))
-f.close()
 
 #######################################
 ##### EQUILIBRIUM
@@ -116,8 +113,14 @@ f.close()
 
 print("\n\n------- Calculating the resting properties.")
 
+# Initial guess for the voltages
+V = -20e-3*np.ones(N,dtype=np.float64)
+#f = open('V0.json','r')
+#V = np.array(json.load(f))
+#f.close()
+
 ## The synaptic activations at rest have a closed form.
-S = Seq()*np.ones(N)
+S = Seq()*np.ones(N,dtype=np.float64)
 
 ## Self-consistently determine the resting membrane potentials
 i = 0
@@ -125,11 +128,11 @@ maxit = 40000
 # Help convergence by taking the average with the K previous steps. This damps
 # the huge oscillations that can build up around the "converged" average value.
 while True:
-    Vold = V
-    V =  Vold+ 1e-3*(Veq(Vold, S) - Vold) 
-    dV = np.abs(V-Vold)
+    Vold = np.copy(V)
+    V =  Vold + 1e-3*(Veq(Vold, S) - Vold) 
+    dV = np.sum(np.abs(V-Vold))
     i+=1
-    if (all(dV<1e-6) or i>maxit): break
+    if (dV<1e-4 or i>maxit): break
     
 # Copy the resting voltages in V0.
 V0 = np.copy(V)
@@ -140,6 +143,7 @@ else:
 
 plt.plot(V0)
 plt.savefig('V0.png')
+plt.clf()
 
 f = open('V0.json','w')
 json.dump(V0.tolist(),f)
@@ -151,16 +155,16 @@ Vth = np.copy(V)
 # Build the full set of degrees of freedom
 Y0 = np.append(V0,S)
 
-
 ######################################
 #### DYNAMICS
 ######################################
 
 print("\n------- Starting with the dynamics.")
 
-Iext = np.zeros((M+1,N))
-#Iext[1:,44] = 1e7 #44 is ASHR
-
+Iext = np.zeros((M+1,N),dtype=np.float64)
+Iext[1:3,44] = 1e1 #44 is ASHR
+#Iext[12:17,44] = 5e4 *C
+#Iext[23:29,44] = 5e4 *C
 
 r = ode(fY).set_integrator('dopri5',nsteps=1000)
 r.set_initial_value(Y0,t0).set_f_params(Vth,Iext)